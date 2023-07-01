d6cdb0d0ce5f92e51f0fc3488d56f34f074424bc
alexb997 <alex_alexandru_997@yahoo.ro>
2023-07-01 19:55:13 +0300

    design update

--- portofolioFront/src/components/pages/home/Home.js
+++ portofolioFront/src/components/pages/home/Home.js
@@ -1,10 +1,26 @@
+import Header from "./Header";
+import Experience from "./Experience";
+import { Container } from "react-bootstrap";
+import "./home.css";
+
 function Home() {
   return (
-    <div>
+    <Container>
       <h1>Bună, sunt Bîlă Alexandru.</h1>
       <h2>Un programator curios axat pe java si javascript.</h2>
       <p>În continuă aspirație spre mai bine.</p>
-    </div>
+      <header className="header"></header>
+      <>
+        <section className="experience">
+          <h2>Experience</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque justo nisl, id pellentesque arcu vestibulum vitae.
+            Aliquam at elit auctor, rhoncus urna vel, eleifend justo.
+          </p>
+        </section>
+      </>
+    </Container>
   );
 }
 
