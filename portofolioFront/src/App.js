cd2c158e82e039597b8654c841476bacb1372adf
alexb997 <alex_alexandru_997@yahoo.ro>
2023-07-07 21:46:27 +0300

    cursor effect incomplete fix

--- portofolioFront/src/App.js
+++ portofolioFront/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import CustomCursorManager from "./components/customCursor/context/manager";
+import CustomCursor from "./components/customCursor/CustomCursor";
 import NavBar from "./components/navigation/NavBar";
 import AppRouter from "./components/routing/AppRouter";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <AppRouter />
+      <CustomCursor />
     </div>
   );
 }
--- portofolioFront/src/components/routing/AppRouter.js
+++ portofolioFront/src/components/routing/AppRouter.js
@@ -2,18 +2,15 @@ import { React } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Projects from "../pages/projects/Projects";
-import CustomCursorManager from "../customCursor/context/manager";
 
 function AppRouter() {
   return (
-    <CustomCursorManager>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </Router>
-    </CustomCursorManager>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
