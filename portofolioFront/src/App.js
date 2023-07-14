d932db164d885959ac3abf551b0d774f8ba23626
alexb997 <alex_alexandru_997@yahoo.ro>
2023-07-14 22:16:08 +0300

    fix cursor

--- portofolioFront/src/App.js
+++ portofolioFront/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import CustomCursor from "./components/customCursor/CustomCursor";
+import NavBar from "./components/navigation/NavBar";
+import AppRouter from "./components/routing/AppRouter";
 
 function App() {
   return (
