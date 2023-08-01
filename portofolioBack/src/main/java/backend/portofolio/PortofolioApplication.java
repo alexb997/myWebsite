03eeaf05ca2727c61bd2bbf5273944fdcf2c3b66
alexb997 <alex_alexandru_997@yahoo.ro>
2023-08-01 20:44:51 +0300

    database error

--- portofolioBack/src/main/java/backend/portofolio/PortofolioApplication.java
+++ portofolioBack/src/main/java/backend/portofolio/PortofolioApplication.java
@@ -2,7 +2,6 @@ package backend.portofolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class PortofolioApplication {
 
